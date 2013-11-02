50ebb4c4395aeefc0ee192f4434753d834a4f849
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-02 04:01:24 -0400

    added socket.io to require.js config

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,25 +1,29 @@
 "use strict";
 
 require.config({
-  paths: {
-    zepto: '3rdparty/zepto.min'
-  , pixi: '3rdparty/pixi'
-  , box2d: '3rdparty/Box2D'
-  },
-  shim: {
-    zepto: {
-      exports: '$'
-    }
-    , pixi: {
-      exports: 'PIXI'
-    }
-    , box2d: {
-      exports: 'Box2D'
-    }
-  }
+	paths: {
+		zepto: '3rdparty/zepto.min',
+		pixi: '3rdparty/pixi',
+		box2d: '3rdparty/Box2D',
+		socketio: '/socket.io/socket.io'
+	},
+	shim: {
+		zepto: {
+			exports: '$'
+		},
+		pixi: {
+			exports: 'PIXI'
+		},
+		box2d: {
+			exports: 'Box2D'
+		},
+		socketio: {
+			exports: 'io'
+		}
+	}
 });
 
 require(['3rdparty/domReady!', 'game'], function(_, G) {
-  window.console = {log: function() {return true;}};
-  G.init();
+	// window.console = {log: function() {return true;}};
+	G.init();
 });
