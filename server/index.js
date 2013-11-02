1080c210ecbb60dfe314a068a5d3e523f320cd58
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-02 03:35:19 -0400

    moved index.html to where it should be: in the public folder

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 server.listen(5000);
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '../index.html');
+    res.sendfile('index.html');
 });
 
 io.sockets.on('connection', function (socket) {
