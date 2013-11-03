5b5d7485b249ed6828286fda2abfb6f52cc8ab0b
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-02 20:37:49 -0400

    refactored code to eliminate verbosity

--- server/friendly-fire.js
+++ server/friendly-fire.js
@@ -2,28 +2,27 @@ var socketio = require('socket.io');
 var Box2D = require('box2dweb');
 var MathUtil = require('./math_helpers');
 
-var ARTIFICIAL_LATENCY_FACTOR = 1; // Make it 1 for no fake latency
+const ARTIFICIAL_LATENCY_FACTOR = 1; // Make it 1 for no fake latency
+const UPDATE_INTERVAL = 1/60;
+const STAGE_WIDTH = 1000; // px
+const STAGE_HEIGHT = 900; // px
+const PX_PER_METER = 100; // conversion 
+var state = {
+	world: null,
+	bodies: [] // instances of b2Body (from Box2D)
+};
+var definitions = {
+	polyFixture: new Box2D.Dynamics.b2FixtureDef(),
+	circleFixture: new Box2D.Dynamics.b2FixtureDef(),
+	bodyDef: new Box2D.Dynamics.b2BodyDef()
+};
+var io = null;
 
 var game = {
-	UPDATE_INTERVAL: 1/60,
-	STAGE_WIDTH: 1000,
-	STAGE_HEIGHT: 900,
-	METER: 100,
-	state: {
-		world: null,
-		bodies: [] // instances of b2Body (from Box2D)
-	},
-	definitions: {
-		polyFixture: new Box2D.Dynamics.b2FixtureDef(),
-		circleFixture: new Box2D.Dynamics.b2FixtureDef(),
-		bodyDef: new Box2D.Dynamics.b2BodyDef()
-	},
 	init: function (server) {
-		var _g = this;
-
-		_g.io = socketio.listen(server);
-		_g.io.set('log level', 1);
-		_g.io.sockets.on('connection', function (socket) {
+		io = socketio.listen(server);
+		io.set('log level', 1);
+		io.sockets.on('connection', function (socket) {
 			console.log("Connection: ", socket.id);
 			socket.on('disconnect', function () {
 				console.log("Disconnect: ", socket.id);
@@ -31,12 +30,12 @@ var game = {
 		});
 		
 		var gravity = new Box2D.Common.Math.b2Vec2(0, 0);
-		_g.state.world = new Box2D.Dynamics.b2World(gravity,  true);
+		state.world = new Box2D.Dynamics.b2World(gravity,  true);
 
 		// TODO: Encapsulate
-		_g.definitions.circleFixture.shape = new Box2D.Collision.Shapes.b2CircleShape();
-		_g.definitions.circleFixture.density = 1;
-		_g.definitions.circleFixture.restitution = 0.7;
+		definitions.circleFixture.shape = new Box2D.Collision.Shapes.b2CircleShape();
+		definitions.circleFixture.density = 1;
+		definitions.circleFixture.restitution = 0.7;
 
 		// const polyFixture = new Box2D.Dynamics.b2FixtureDef()
 		// polyFixture.shape = new Box2D.Collision.Shapes.b2PolygonShape();
@@ -52,65 +51,63 @@ var game = {
 
 		//down
 		// polyFixture.shape.SetAsBox(10, 1);
-		// bodyDef.position.Set(9, _g.STAGE_HEIGHT / _g.METER + 1);
-		// _g.state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
+		// bodyDef.position.Set(9, STAGE_HEIGHT / PX_PER_METER + 1);
+		// state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
 
 		//left
 		// polyFixture.shape.SetAsBox(1, 100);
 		// bodyDef.position.Set(-1, 0);
-		// _g.state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
+		// state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
 
 		//right
-		// bodyDef.position.Set(_g.STAGE_WIDTH / _g.METER + 1, 0);
-		// _g.state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
+		// bodyDef.position.Set(STAGE_WIDTH / PX_PER_METER + 1, 0);
+		// state.world.CreateBody(bodyDef).CreateFixture(polyFixture);
 		// bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
 
 		// for (var i = 0; i < 40; i++) {
-		// 	bodyDef.position.Set(MathUtil.rndRange(0, _g.STAGE_WIDTH) / _g.METER, -MathUtil.rndRange(50, 5000) / _g.METER);
-		// 	var body = _g.state.world.CreateBody(bodyDef);
+		// 	bodyDef.position.Set(MathUtil.rndRange(0, STAGE_WIDTH) / PX_PER_METER, -MathUtil.rndRange(50, 5000) / PX_PER_METER);
+		// 	var body = state.world.CreateBody(bodyDef);
 		// 	var s;
 		// 	if (i/40 > 0.5) {
 		// 		s = i/40*50+50;
-		// 		circleFixture.shape.SetRadius(s / 2 / _g.METER);
+		// 		circleFixture.shape.SetRadius(s / 2 / PX_PER_METER);
 		// 		body.CreateFixture(circleFixture);
-		// 		_g.state.bodies.push(body);
+		// 		state.bodies.push(body);
 		// 	}
 		// 	else {
 		// 		s = i/40*50+50;
-		// 		polyFixture.shape.SetAsBox(s / 2 / _g.METER, s / 2 / _g.METER);
+		// 		polyFixture.shape.SetAsBox(s / 2 / PX_PER_METER, s / 2 / PX_PER_METER);
 		// 		body.CreateFixture(polyFixture);
-		// 		_g.state.bodies.push(body);
+		// 		state.bodies.push(body);
 		// 	}
 		// }
 
-		_g.addShip({pos: {x: _g.STAGE_WIDTH/2/_g.METER, y: _g.STAGE_HEIGHT/2/_g.METER}});
+		this.addShip({pos: {x: STAGE_WIDTH/2/PX_PER_METER, y: STAGE_HEIGHT/2/PX_PER_METER}});
 
-		setInterval(_g.step.bind(_g), _g.UPDATE_INTERVAL * 1000);
-		setInterval(_g.sync.bind(_g), _g.UPDATE_INTERVAL * 1000 * ARTIFICIAL_LATENCY_FACTOR);
+		setInterval(this.step.bind(this), UPDATE_INTERVAL * 1000);
+		setInterval(this.sync.bind(this), UPDATE_INTERVAL * 1000 * ARTIFICIAL_LATENCY_FACTOR);
 	},
 	addShip: function (params) {
 		params = params || {};		
-		var _g = this,
-				pos = params.pos || {x: 0, y: 0},
-				body,
-				size;
+		var pos = params.pos || {x: 0, y: 0},
+			body,
+			size;
 
-		_g.definitions.bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
-		_g.definitions.bodyDef.position.Set(pos.x, pos.y);
-		body = _g.state.world.CreateBody(_g.definitions.bodyDef);
+		definitions.bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+		definitions.bodyDef.position.Set(pos.x, pos.y);
+		body = state.world.CreateBody(definitions.bodyDef);
 
 		size = 50;
-		_g.definitions.circleFixture.shape.SetRadius(size / 2 / _g.METER);
-		body.CreateFixture(_g.definitions.circleFixture);
+		definitions.circleFixture.shape.SetRadius(size / 2 / PX_PER_METER);
+		body.CreateFixture(definitions.circleFixture);
 
-		_g.state.bodies.push(body);
+		state.bodies.push(body);
 	},
 	sync: function () {
-		var _g = this,
-				data = [];
-		const n = _g.state.bodies.length;
+		var data = [];
+		const n = state.bodies.length;
 		for (var i = 0; i < n; i++) {
-			var body  = _g.state.bodies[i];
+			var body  = state.bodies[i];
 			var position = body.GetPosition();
 			var velocity = body.GetLinearVelocity();
 			var angular_velocity = body.GetAngularVelocity();
@@ -121,13 +118,12 @@ var game = {
 									y_vel: velocity.y,
 									a_vel: angular_velocity });
 		}
-		_g.io.sockets.emit('update', data);
-		// console.log(_g.state.world.m_island.m_bodies);
+		io.sockets.emit('update', data);
+		// console.log(state.world.m_island.m_bodies);
 	},
 	step: function () {
-		var _g = this;
-		_g.state.world.Step(_g.UPDATE_INTERVAL, 3, 3);
-		_g.state.world.ClearForces();
+		state.world.Step(UPDATE_INTERVAL, 3, 3);
+		state.world.ClearForces();
 	}
 };
 
